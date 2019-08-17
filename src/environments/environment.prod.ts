bacbfd1670428559a6fd459c9384c266ddf8426a
Charly921 <44937626+Charly921@users.noreply.github.com>
2019-08-16 23:31:58 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyAr05bQk5FMZaO1FR2obJBNsrKZUZIwblY',
-    authDomain: 'multiacabados-ed560.firebaseapp.com',
-    databaseURL: 'https://multiacabados-ed560.firebaseio.com',
-    projectId: 'multiacabados-ed560',
-    storageBucket: '',
-    messagingSenderId: '885571252463',
-    appId: '1:885571252463:web:b291b13acd6147af'
-  }
+  production: true
 };
